cffe402b3306bf8c7e3280305f4f8a7faf12c560
pattakaree <prot@givingeco.com>
2022-04-23 18:17:27 +0700

    Add new one api called "sawatdee"

--- index.py
+++ index.py
@@ -5,17 +5,25 @@ from fastapi import APIRouter
 # Start Create Asyn Function
 helloapi = APIRouter()
 goodbyeapi = APIRouter()
+sawatdeeapi = APIRouter()
 
 
 @helloapi.get("/hello")
 async def hello():
     return 'Hello'
 
+
 @goodbyeapi.get("/goodbye")
 async def goodbye():
     return 'GoodBye'
+
+
+@sawatdeeapi.get("/sawatdee")
+async def sawatdee():
+    return 'Sawatdee'
 
 # Main App
 app = FastAPI()
 app.include_router(helloapi)
 app.include_router(goodbyeapi)
+app.include_router(sawatdeeapi)
